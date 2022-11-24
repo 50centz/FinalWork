4418f512d1ed3da103e9d4a4843c2d5af688ef3e
Федоров Евгений <zen-g@mail.ru>
2022-11-24 11:28:08 +0300

    Create array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateNewArray()
+{   
+    Console.WriteLine("Enter the size of the array: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Enter the word: ");
+        string a = Console.ReadLine();
+        array[i] = a;
+    }
+
+
+    return array;
+}
